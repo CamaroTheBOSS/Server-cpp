41429c09d1f857bffb62c990a5d2b2aa1d0ea63b
CamaroTheBOSS <plessico@wp.pl>
2024-09-30 20:22:25 +0200

    Introduced simple, single client TCP server

---
+++ Client/Client.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <string>
+#pragma comment(lib, "Ws2_32.lib")
+
+class Client {
+public:
+    Client(std::string&& srvIpAddress, const int srvPort) :
+        srvIpAddress(srvIpAddress),
+        srvPort(srvPort) {
+        initWSA();
+        openClientSocket();
+    }
+
+    int connectToServer() {
+        clientService.sin_family = AF_INET;
+        std::wstring stemp{srvIpAddress.begin(), srvIpAddress.end()};
+        InetPton(AF_INET, stemp.c_str(), &clientService.sin_addr.s_addr);
+        clientService.sin_port = htons(srvPort);
+
+        if (connect(clientSocket, reinterpret_cast<SOCKADDR*>(&clientService), sizeof(clientService)) == SOCKET_ERROR) {
+            return handleError("[CONNECTION ERROR]");
+        }
+        handleConnection();
+        return 0;
+    }
+
+private:
+    int handleError(std::string&& prefix) {
+        std::cout << prefix << ' ' << WSAGetLastError() << '\n';
+        closesocket(clientSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    void shutdownConn(SOCKET& connSocket) {
+        int shutDownResult = shutdown(connSocket, SD_SEND);
+        closesocket(connSocket);
+    }
+
+    void handleConnection() {
+        char sendBuffer[200];
+        char receiveBuffer[200];
+        int recvByteCount = 0;
+        do {
+            std::cin.getline(sendBuffer, 200);
+            int sendByteCount = send(clientSocket, sendBuffer, 200, 0);
+            if (sendByteCount == SOCKET_ERROR) {
+                shutdownConn(clientSocket);
+                handleError("[SEND ERROR]");
+                return;
+            }
+            recvByteCount = recv(clientSocket, receiveBuffer, 200, 0);
+            if (recvByteCount == 0) {
+                std::cout << "Closing connection\n";
+                shutdownConn(clientSocket);
+            }
+            else if (recvByteCount < 0){
+                shutdownConn(clientSocket);
+                handleError("[RECV ERROR]");
+            }
+        } while (recvByteCount > 0);
+        return;
+    }
+
+    int initWSA() {
+        WSADATA wsaData;
+        int wsaError;
+        WORD mVersionRequired = MAKEWORD(2, 2);
+        wsaError = WSAStartup(mVersionRequired, &wsaData);
+        if (wsaError) {
+            std::cout << "[ERROR:" + std::to_string(wsaError) + "] Error on WSAStartup.\n";
+            return 0;
+        }
+        else {
+            std::cout << "Winsock properly started. Status " << wsaData.szSystemStatus << "\n";
+        }
+    }
+
+    int openClientSocket() {
+        clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (clientSocket == INVALID_SOCKET) {
+            std::cout << "[SOCKET OPENING ERROR] " << WSAGetLastError() << "\n";
+            closesocket(clientSocket);
+            WSACleanup();
+            return 0;
+        }
+    }
+
+    std::string srvIpAddress;
+    int srvPort;
+
+    SOCKET clientSocket;
+    sockaddr_in clientService;
+};
+
+int main()
+{
+    Client client{ "127.0.0.1", 8081 };
+    client.connectToServer();
+    std::cout << "SHUTDOWN";
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,119 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <string>
+#pragma comment(lib, "Ws2_32.lib")
+
+class Server {
+
+public:
+    Server(std::string&& ipAddress, const int port):
+        ipAddress(std::move(ipAddress)),
+        port(port) {
+        initWSA();
+        openServerSocket();
+        bindServerSocket();
+    }
+
+    int listenForConns() {
+        if (listen(serverSocket, 1) == SOCKET_ERROR) {
+            return handleError("[LISTENING ERROR]");
+        }
+        addNewConn();
+        return listenForConns();   
+    }
+
+private:
+    int addNewConn() {
+        SOCKET connSocket = accept(serverSocket, nullptr, nullptr);
+        if (connSocket == INVALID_SOCKET) {
+            closesocket(connSocket);
+            return handleError("[CONNECTION REFUSED]");
+        }
+        handleConnection(connSocket);
+        return 0;
+    }
+
+    int handleError(std::string&& prefix) {
+        std::cout << prefix << ' ' << WSAGetLastError() << '\n';
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    void shutdownConn(SOCKET& connSocket) {
+        int shutDownResult = shutdown(connSocket, SD_SEND);
+        closesocket(connSocket);
+    }
+
+    int handleConnection(SOCKET& connSocket) {
+        char recvBuffer[200];
+        int recvByteCount = 0;
+        do {
+            recvByteCount = recv(connSocket, recvBuffer, 200, 0);
+            if (recvByteCount > 0) {
+                std::cout << recvBuffer << "\n";
+                int sendByteCount = send(connSocket, recvBuffer, 200, 0);
+                if (sendByteCount == SOCKET_ERROR) {
+                    shutdownConn(connSocket);
+                    handleError("[SEND ERROR]");
+                }
+            }
+            else if (recvByteCount == 0){
+                std::cout << "Closing connection\n";
+                shutdownConn(connSocket);
+            }
+            else {
+                shutdownConn(connSocket);
+                handleError("[RECV ERROR]");
+            }
+        } while (recvByteCount > 0);
+        return 0;
+    }
+
+    int initWSA() {
+        WSADATA wsaData;
+        int wsaError;
+        WORD wVersionRequested = MAKEWORD(2, 2);
+        wsaError = WSAStartup(wVersionRequested, &wsaData);
+        if (wsaError) {
+            std::cout << "[ERROR:" + std::to_string(wsaError) + "] Error on WSAStartup.\n";
+            return -1;
+        }
+        else {
+            std::cout << "Winsock properly started. Status " << wsaData.szSystemStatus << "\n";
+        }
+        return 0;
+    }
+
+    int openServerSocket() {
+        serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (serverSocket == INVALID_SOCKET) {
+            return handleError("[SOCKET OPENING ERROR]");
+        }
+        return 0;
+    }
+
+    int bindServerSocket() {
+        service.sin_family = AF_INET;
+        std::wstring stemp{ipAddress.begin(), ipAddress.end()};
+        InetPton(AF_INET, stemp.c_str(), &service.sin_addr.s_addr);
+        service.sin_port = htons(port);
+        if (bind(serverSocket, reinterpret_cast<SOCKADDR*>(&service), sizeof(service)) == SOCKET_ERROR) {
+            return handleError("[SOCKET BINDING ERROR]");
+        }
+        return 0;
+    }
+
+    std::string ipAddress;
+    int port;
+
+    SOCKET serverSocket = INVALID_SOCKET;
+    sockaddr_in service;
+};
+
+int main()
+{
+    Server server{ "127.0.0.1", 8081 };
+    server.listenForConns();
+}
