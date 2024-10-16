7b61a291f9778eafd059e22ad42de7bb27e24478
CamaroTheBOSS <plessico@wp.pl>
2024-10-16 14:10:35 +0200

    Created generic database class and provided tests with Users and Docs tables

--- Server/database.h
+++ Server/database.h
@@ -2,87 +2,248 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <array>
+#include <sstream>
 
 #include "logger.h"
 
 namespace db {
 
-	namespace random {
-		std::string generateUUID();
-	}
-
+	std::string generateUUID();
 	bool validateStrField(const std::string& str, const std::string& notAllowedLetters);
 
+	template<int... UNIQUENESS_MASK>
+	struct Data {
+	public:
+		Data(const std::string& uuid, const std::string& name) :
+			uuid(uuid),
+			name(name) {}
+		virtual bool valid() const = 0;
+		virtual std::string str() const = 0;
+		virtual std::vector<std::string> row() const = 0;
+		const std::array<bool, sizeof...(UNIQUENESS_MASK)>& uniqueMask() const {
+			return uniqueKeys;
+		}
+		const std::string uuid;
+		const std::string name;
+		static constexpr std::array<bool, sizeof...(UNIQUENESS_MASK)> uniqueKeys = { UNIQUENESS_MASK... };
+	
+	};
 
-struct UserData {
-	UserData(const std::string& username, const std::string& password) :
-		username(username),
-		password(password) {}
-	UserData(const std::string uuid, std::string& username, const std::string& password) :
-		uuid(uuid),
-		username(username),
-		password(password) {}
+	struct User: public Data<1, 1, 0> {
+		User() :
+			Data("", "USER"),
+			username(""),
+			password("") {}
+		User(const std::string& username, const std::string& password) :
+			Data("", "USER"),
+			username(username),
+			password(password) {}
+		User(const std::string& uuid, const User& user) :
+			Data(uuid, "USER"),
+			username(user.username),
+			password(user.password) {}
+		User(const std::vector<std::string>& row) :
+			Data(row[0], "USER"),
+			username(row[1]),
+			password(row[2]) {}
 
-	bool valid() const {
-		if (!validateStrField(username, ",\n") || !validateStrField(password, ",\n")) {
+		bool valid() const override {
+			if (!validateStrField(username, ",\n") || !validateStrField(password, ",\n")) {
+				return false;
+			}
+			return true;
+		}
+		std::string str() const override {
+			return uuid + "," + username + "," + password;
+		}
+		std::vector<std::string> row() const override {
+			return { uuid, username, password };
+		}
+		std::string username;
+		std::string password;
+	};
+
+	struct Doc: public Data<1, 0> {
+		Doc() :
+			Data("", "DOC"),
+			filename("") {}
+		Doc(const std::string& filename) :
+			Data("", "DOC"),
+			filename(filename) {}
+		Doc(const std::string& uuid, const Doc& doc) :
+			Data(uuid, "DOC"),
+			filename(doc.filename) {}
+		Doc(const std::vector<std::string>& row) :
+			Data(row[0], "DOC"),
+			filename(row[1]) {}
+
+		bool valid() const override {
+			if (!validateStrField(filename, ",\n")) {
+				return false;
+			}
+			return true;
+		}
+		std::string str() const override {
+			return uuid + "," + filename;
+		}
+		std::vector<std::string> row() const override {
+			return { uuid, filename };
+		}
+		std::string filename;
+	};
+
+	template<typename OBJ>
+	class Database {
+	public:
+		Database(std::string& dbPath, logs::Logger& logger):
+			dbPath(dbPath),
+			logger(logger) {};
+
+		const std::string create(OBJ& obj) {
+			if (!obj.valid()) {
+				logger.log(logs::Level::ERROR, obj.name + "is not valid: " + obj.str());
+				return "";
+			}
+
+			std::ofstream db(dbPath, std::ostream::out | std::ostream::app);
+			if (!db) {
+				logger.log(logs::Level::ERROR, "Cannot open: " + dbPath);
+				return "";
+			}
+
+			std::string uuid = obj.uuid.empty() ? generateUUID() : obj.uuid;
+			OBJ objToDb{ uuid, obj };
+			if (checkUniqueness(objToDb)) {
+				db << objToDb.str() << "\n" << std::flush;
+				logger.log(logs::Level::INFO, obj.name + "added: " + obj.str());
+				return uuid;
+			}
+			return "";
+		}
+
+		OBJ read(const std::string& uuid) {
+			auto rowDb = getRowWithUuid(uuid);
+			if (rowDb.empty()) {
+				return OBJ{};
+			}
+			return OBJ{ rowDb };
+		}
+		
+		bool update(const OBJ& newObj) {
+			if (!newObj.valid()) {
+				logger.log(logs::Level::ERROR, newObj.name + "is not valid: " + newObj.str());
+				return false;
+			}				
+			if (editRowWithUuid(newObj.uuid, newObj.str() + "\n")) {
+				logger.log(logs::Level::INFO, newObj.name + " updated: " + newObj.str());
+				return true;
+			}
+			logger.log(logs::Level::INFO, newObj.name + " not found: " + newObj.str());
 			return false;
 		}
-		return true;
-	}
-	std::string toString() const {
-		return uuid + "," + username + "," + password;
-	}
-	const std::string uuid = "";
-	std::string username;
-	std::string password;
-};
 
-struct DocData {
-	DocData(const std::string& filename) :
-		filename(filename) {}
-	DocData(const std::string uuid, const std::string& filename) :
-		uuid(uuid),
-		filename(filename) {}
-
-	bool valid() const {
-		if (!validateStrField(filename, ",\n")) {
+		bool erase(const std::string& uuid) {
+			if (editRowWithUuid(uuid, "")) {
+				logger.log(logs::Level::INFO, uuid + " deleted from " + dbPath);
+				return true;
+			}
+			logger.log(logs::Level::INFO, "Obj not found: " + uuid + " in " + dbPath);
 			return false;
 		}
-		return true;
-	}
-	std::string toString() const {
-		return uuid + "," + filename;
-	}
-	const std::string uuid = "";
-	std::string filename;
-};
 
-class Database {
-public:
-	Database(std::string& root, logs::Logger& logger);
-	std::string createUser(UserData& userData) const;
-	std::string createDoc(DocData& docData) const;
+	private:
+		bool editRowWithUuid(const std::string& uuid, const std::string& newRow) const {
+			std::ifstream dbIn(dbPath, std::ios::in);
+			if (!dbIn) {
+				logger.log(logs::Level::ERROR, "Cannot open: " + dbPath);
+				return false;
+			}
 
-	UserData readUser(const std::string& userId) const;
-	DocData readDoc(const std::string& docId) const;
+			std::string row;
+			std::vector<std::string> lines;
+			while (std::getline(dbIn, row)) {
+				lines.push_back(row);
+			}
+			dbIn.close();
 
-	bool updateUser(const UserData& userData) const;
-	bool updateDoc(const DocData& docData) const;
+			std::string fileContent;
+			for (auto& line : lines) {
+				std::string rowUuid = line.substr(0, line.find(","));
+				if (rowUuid == uuid) {
+					fileContent += newRow;
+				}
+				else {
+					fileContent += line + '\n';
+				}
+			}
 
-	bool deleteUser(const std::string& userId) const;
-	bool deleteDoc(const std::string& docId) const;
+			std::ofstream dbOut(dbPath, std::ios::out);
+			if (!dbOut) {
+				logger.log(logs::Level::ERROR, "Cannot open: " + dbPath);
+				return false;
+			}
+			dbOut << fileContent;
+			return true;
+		}
 
-private:
-	bool editRowWithUuid(const std::string& dbFile, const std::string& uuid, const std::string& newLine) const;
-	std::vector<std::string> findRowWithUuid(const std::string& dbFile, const std::string& uuid) const;
-	std::vector<std::string> parseRow(std::string& row, char delimiter) const;
+		std::vector<std::string> getRowWithUuid(const std::string& uuid) {
+			std::ifstream db(dbPath, std::istream::in);
+			if (!db) {
+				logger.log(logs::Level::ERROR, "Cannot open: " + dbPath + " for read");
+				return {};
+			}
 
-	std::string root;
-	std::string userDb;
-	std::string docDb;
+			std::string rowStr;
+			while (std::getline(db, rowStr)) {
+				std::string rowUuid = rowStr.substr(0, rowStr.find(","));
+				if (rowUuid == uuid) {
+					return parseRow(rowStr, ',');
+				}
+			}
+			logger.log(logs::Level::DEBUG, "Not found obj with uuid: " + uuid + " from db", dbPath);
+			return {};
+		}
 
-	logs::Logger& logger;
-};
+		bool checkUniqueness(const OBJ& obj) const {
+			std::ifstream db(dbPath, std::istream::in);
+			if (!db) {
+				logger.log(logs::Level::ERROR, "Cannot open: " + dbPath + " for read");
+				return "";
+			}
 
+			auto rowObj = obj.row();
+			auto uniqueMask = obj.uniqueMask();
+			std::string rowStr;
+			while (std::getline(db, rowStr)) {
+				auto rowDb = parseRow(rowStr, ',');
+				for (int i = 0; i < rowObj.size(); i++) {
+					if (!uniqueMask[i]) {
+						continue;
+					}
+					if (rowDb[i] == rowObj[i]) {
+						logger.log(logs::Level::ERROR, "Such object already exists in db " + dbPath + ": " + obj.str());
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+		std::vector<std::string> parseRow(std::string& row, char delimiter) const {
+			std::vector<std::string> parsedRow;
+			int offset = 0;
+			int delimiterPos = 0;
+			while ((delimiterPos = row.find(delimiter, offset)) != std::string::npos) {
+				parsedRow.emplace_back(row.substr(offset, delimiterPos - offset));
+				offset = delimiterPos + 1;
+			}
+			parsedRow.emplace_back(row.substr(offset, row.size() - offset));
+			return parsedRow;
+		}
+
+		std::string dbPath;
+		logs::Logger& logger;
+	};
 
 }
--- Test/database_test.cpp
+++ Test/database_test.cpp
@@ -3,45 +3,245 @@
 
 #include "database.h"
 
-TEST(CRUDUserTest, DatabaseTests) {
-	const std::string username = "username";
-	const std::string updatedUsername = "updatedUsername";
-	const std::string password = "password";
-	std::string root = ".";
+TEST(CreateUserTest, DatabaseTests) {
+	std::string dbPath = "CreateUserTest.csv";
 	logs::Logger logger("test.log");
-	db::Database db{ root, logger};
+	db::Database<db::User> db{dbPath, logger};
 
-	db::UserData userData{ username, password };
-	std::string userId = db.createUser(userData);
-	EXPECT_FALSE(userId.empty());
-
-	db::UserData readUserData = db.readUser(userId);
-	EXPECT_EQ(readUserData.uuid, userId);
-	EXPECT_EQ(readUserData.username, username);
-	EXPECT_EQ(readUserData.password, password);
-
-	readUserData.username = updatedUsername;
-	EXPECT_TRUE(db.updateUser(readUserData));
-
-	db::UserData updatedUserData = db.readUser(userId);
-	EXPECT_EQ(readUserData.uuid, userId);
-	EXPECT_EQ(readUserData.username, updatedUsername);
-	EXPECT_EQ(readUserData.password, password);
-
-	EXPECT_TRUE(db.deleteUser(userId));
-	EXPECT_FALSE(db.deleteUser(userId));
+	const std::string username = "username";
+	const std::string password = "password";
+	
+	db::User userData{ username, password };
+	std::string uuid = db.create(userData);
+	EXPECT_FALSE(uuid.empty());
+	EXPECT_FALSE(std::remove(dbPath.c_str()));
 }
 
-TEST(CRUDDocTest, DatabaseTests) {
-	const std::string filename = "filename.txt";
-	std::string root = ".";
+TEST(CreateUserWithTheSameUuidTest, DatabaseTests) {
+	std::string dbPath = "CreateUserWithTheSameUuidTest.csv";
 	logs::Logger logger("test.log");
-	db::Database db{ root, logger};
+	db::Database<db::User> db{dbPath, logger};
 
-	db::DocData docData{ filename };
-	std::string docId = db.createDoc(docData);
-	EXPECT_FALSE(docId.empty());
-	db::DocData readDocData = db.readDoc(docId);
-	EXPECT_EQ(readDocData.uuid, docId);
-	EXPECT_EQ(readDocData.filename, filename);
+	const std::string username = "username";
+	const std::string password = "password";
+
+	db::User user{ username, password };
+	std::string uuid = db.create(user);
+	EXPECT_FALSE(uuid.empty());
+
+	db::User nextUser{uuid, user};
+	uuid = db.create(nextUser);
+	EXPECT_TRUE(uuid.empty());
+	EXPECT_FALSE(std::remove(dbPath.c_str()));
+}
+
+TEST(CreateUserWithTheSameUsernameTest, DatabaseTests) {
+	std::string dbPath = "CreateUserWithTheSameUsernameTest.csv";
+	logs::Logger logger("test.log");
+	db::Database<db::User> db{dbPath, logger};
+
+	const std::string username = "username";
+	const std::string password = "password";
+
+	db::User user{ username, password };
+	std::string uuid = db.create(user);
+	EXPECT_FALSE(uuid.empty());
+
+	db::User nextUser{username, password};
+	uuid = db.create(user);
+	EXPECT_TRUE(uuid.empty());
+	EXPECT_FALSE(std::remove(dbPath.c_str()));
+}
+
+TEST(CreateMultipleUsersTest, DatabaseTests) {
+	std::string dbPath = "CreateMultipleUsersTest.csv";
+	logs::Logger logger("test.log");
+	db::Database<db::User> db{dbPath, logger};
+
+	const std::string baseUsername = "username";
+	const std::string password = "password";
+
+	for (int i = 0; i < 10; i++) {
+		db::User user{ baseUsername + std::to_string(i), password };
+		std::string uuid = db.create(user);
+		EXPECT_FALSE(uuid.empty());
+	}
+	EXPECT_FALSE(std::remove(dbPath.c_str()));
+}
+
+TEST(CreateDocTest, DatabaseTests) {
+	std::string dbPath = "CreateDocTest.csv";
+	logs::Logger logger("test.log");
+	db::Database<db::Doc> db{dbPath, logger};
+
+	const std::string filename = "filename.txt";
+	db::Doc doc{ filename };
+	std::string uuid = db.create(doc);
+	EXPECT_FALSE(uuid.empty());
+	EXPECT_FALSE(std::remove(dbPath.c_str()));
+}
+
+TEST(CreateDocWithTheSameUuidTest, DatabaseTests) {
+	std::string dbPath = "CreateDocWithTheSameUuidTest.csv";
+	logs::Logger logger("test.log");
+	db::Database<db::Doc> db{dbPath, logger};
+
+	const std::string filename = "filename.txt";
+	db::Doc doc{ filename };
+	std::string uuid = db.create(doc);
+	EXPECT_FALSE(uuid.empty());
+
+	db::Doc nextDoc{uuid, doc};
+	uuid = db.create(nextDoc);
+	EXPECT_TRUE(uuid.empty());
+	EXPECT_FALSE(std::remove(dbPath.c_str()));
+}
+
+TEST(CreateMultipleDocsTest, DatabaseTests) {
+	std::string dbPath = "CreateMultipleDocsTest.csv";
+	logs::Logger logger("test.log");
+	db::Database<db::Doc> db{dbPath, logger};
+
+	const std::string baseFilename = "filename.txt";
+	for (int i = 0; i < 10; i++) {
+		db::Doc doc{ std::to_string(i) + baseFilename };
+		std::string uuid = db.create(doc);
+		EXPECT_FALSE(uuid.empty());
+	}
+	EXPECT_FALSE(std::remove(dbPath.c_str()));
+}
+
+TEST(ReadUserTest, DatabaseTests) {
+	std::string uuid = "0081aa72-f4a9-4835-990c-50dbfdb0279c";
+	std::string dbPath = "ReadUserTest.csv";
+	logs::Logger logger("test.log");
+	db::Database<db::User> db{dbPath, logger};
+
+	db::User user = db.read(uuid);
+	EXPECT_EQ(user.uuid, uuid);
+	EXPECT_EQ(user.username, "username5");
+	EXPECT_EQ(user.password, "password");
+}
+
+TEST(ReadDocTest, DatabaseTests) {
+	std::string uuid = "41d6c3c0-e1a0-4ade-882c-7b0f64df8d9f";
+	std::string dbPath = "ReadDocTest.csv";
+	logs::Logger logger("test.log");
+	db::Database<db::Doc> db{dbPath, logger};
+
+	db::Doc doc = db.read(uuid);
+	EXPECT_EQ(doc.uuid, uuid);
+	EXPECT_EQ(doc.filename, "5filename.txt");
+}
+
+TEST(UpdateUserTest, DatabaseTests) {
+	std::string dbPath = "UpdateUserTest.csv";
+	std::string uuid = "9f3d9e66-af0e-4f66-bbbc-c714f4c40ebd";
+	std::string updatedUsername = "updatedUsername";
+
+	logs::Logger logger("test.log");
+	db::Database<db::User> db{dbPath, logger};
+
+	std::ofstream dbFstream(dbPath, std::ostream::out);
+	std::string dbContent =
+		"f11d49e1-e0c5-43cd-806d-4a9a50042696,username0,password\n"
+		"9f3d9e66-af0e-4f66-bbbc-c714f4c40ebd,username1,password\n"
+		"214bae8c-218d-474e-92d0-56dc344f3112,username2,password\n"
+		"6566adb8-1427-4433-be38-90be6b5a5db7,username3,password\n";
+	dbFstream << dbContent;
+	dbFstream.close();
+
+	db::User user = db.read(uuid);
+	std::vector<std::string> row = { user.uuid, updatedUsername, user.password };
+	db::User newUser{row};
+	bool updated = db.update(newUser);
+	db::User updatedUser = db.read(uuid);
+
+	EXPECT_TRUE(updated);
+	EXPECT_EQ(updatedUser.uuid, uuid);
+	EXPECT_EQ(updatedUser.uuid, user.uuid);
+	EXPECT_EQ(updatedUser.username, updatedUsername);
+	EXPECT_EQ(updatedUser.password, user.password);
+	EXPECT_FALSE(std::remove(dbPath.c_str()));
+}
+
+TEST(UpdateDocTest, DatabaseTests) {
+	std::string dbPath = "UpdateDocTest.csv";
+	std::string uuid = "babd37e1-e19d-463f-a5af-fcbba13296aa";
+	std::string updatedFilename = "updatedFilename.txt";
+
+	logs::Logger logger("test.log");
+	db::Database<db::Doc> db{dbPath, logger};
+
+	std::ofstream dbFstream(dbPath, std::ostream::out);
+	std::string dbContent =
+		"152fbabb-b49c-4f01-a94c-cde5c628ec55,0filename.txt\n"
+		"586f7fbb-fcff-4d43-8031-cec2ba24003e,1filename.txt\n"
+		"7a8d33a8-134a-4845-95ec-f78d35c0b35f,2filename.txt\n"
+		"babd37e1-e19d-463f-a5af-fcbba13296aa,3filename.txt\n";
+	dbFstream << dbContent;
+	dbFstream.close();
+
+	db::Doc doc = db.read(uuid);
+	std::vector<std::string> row = { doc.uuid, updatedFilename };
+	db::Doc newDoc{row};
+	bool updated = db.update(newDoc);
+	db::Doc updatedDoc = db.read(uuid);
+
+	EXPECT_TRUE(updated);
+	EXPECT_EQ(updatedDoc.uuid, uuid);
+	EXPECT_EQ(updatedDoc.uuid, doc.uuid);
+	EXPECT_EQ(updatedDoc.filename, updatedFilename);
+	EXPECT_FALSE(std::remove(dbPath.c_str()));
+}
+
+TEST(DeleteUserTest, DatabaseTests) {
+	std::string dbPath = "DeleteUserTest.csv";
+	std::string uuid = "9f3d9e66-af0e-4f66-bbbc-c714f4c40ebd";
+
+	logs::Logger logger("test.log");
+	db::Database<db::User> db{dbPath, logger};
+
+	std::ofstream dbFstream(dbPath, std::ostream::out);
+	std::string dbContent =
+		"f11d49e1-e0c5-43cd-806d-4a9a50042696,username0,password\n"
+		"9f3d9e66-af0e-4f66-bbbc-c714f4c40ebd,username1,password\n"
+		"214bae8c-218d-474e-92d0-56dc344f3112,username2,password\n"
+		"6566adb8-1427-4433-be38-90be6b5a5db7,username3,password\n";
+	dbFstream << dbContent;
+	dbFstream.close();
+
+	bool deleted = db.erase(uuid);
+	db::User user = db.read(uuid);
+
+	EXPECT_TRUE(deleted);
+	EXPECT_EQ(user.uuid, "");
+	EXPECT_EQ(user.username, "");
+	EXPECT_EQ(user.password, "");
+	EXPECT_FALSE(std::remove(dbPath.c_str()));
+}
+
+TEST(DeleteDocTest, DatabaseTests) {
+	std::string dbPath = "DeleteDocTest.csv";
+	std::string uuid = "babd37e1-e19d-463f-a5af-fcbba13296aa";
+
+	logs::Logger logger("test.log");
+	db::Database<db::Doc> db{dbPath, logger};
+
+	std::ofstream dbFstream(dbPath, std::ostream::out);
+	std::string dbContent =
+		"152fbabb-b49c-4f01-a94c-cde5c628ec55,0filename.txt\n"
+		"586f7fbb-fcff-4d43-8031-cec2ba24003e,1filename.txt\n"
+		"7a8d33a8-134a-4845-95ec-f78d35c0b35f,2filename.txt\n"
+		"babd37e1-e19d-463f-a5af-fcbba13296aa,3filename.txt\n";
+	dbFstream << dbContent;
+	dbFstream.close();
+
+	bool deleted = db.erase(uuid);
+	db::Doc doc = db.read(uuid);
+
+	EXPECT_TRUE(deleted);
+	EXPECT_EQ(doc.uuid, "");
+	EXPECT_EQ(doc.filename, "");
+	EXPECT_FALSE(std::remove(dbPath.c_str()));
 }
